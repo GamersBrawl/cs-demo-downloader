ef8eea4e45acf5e3d9e9abeb432040a2181ef5bb
Felipe Bolonhani <bolonhanifoxx@gmail.com>
2026-01-21 10:49:58 -0300

    fix: call gamersbrawl function on load config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { readJSONSync } from 'fs-extra/esm';
 import path from 'node:path';
-import axios, { isAxiosError } from 'axios';
+import { isAxiosError } from 'axios';
+import { getAuthCodes } from './gamersbrawl.js';
 
 export interface Config {
   authCodes?: AuthCodeUser[];
@@ -31,13 +32,7 @@ const configFile = path.join(configDir, 'config.json');
 
 export const config = readJSONSync(configFile, 'utf-8') as Config;
 try {
-  // get base url from environment variable
-  const url = 'https://ys440sg844kg8oockc4cwsos.is-on.cloud/api/v1/counter_strike_users/';
-  const headers = {
-    'X-API-Token': '117cb41fec941f9d746e1a9f73197d14f3f8c2fb5aa301ec6a49baef2db0bb7a',
-  };
-  const response = await axios.get(url, { headers });
-  config.authCodes = response.data;
+  config.authCodes = await getAuthCodes();
 } catch (e) {
   if (isAxiosError(e)) {
     console.error('Axios error', e.message);
